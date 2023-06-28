3446b814f78bd874a8119c7a833be91409670f06
Parsa Frahani <90212907+parsaFarahani84@users.noreply.github.com>
2023-06-28 12:35:10 +0330

    finish

--- shopside/src/components/Products/SingleProduct.jsx
+++ shopside/src/components/Products/SingleProduct.jsx
@@ -1,6 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import "./SinglePage.css";
+import { CgDollar } from "react-icons/cg";
+import { BsBack } from "react-icons/bs";
+import { TiArrowBack } from "react-icons/ti";
+import { SiChainlink } from "react-icons/si";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function SingleProduct() {
   const [real, setReal] = useState([]);
@@ -12,7 +19,51 @@ function SingleProduct() {
       .then((i) => setReal(i.data));
   }, []);
 
-  return <div></div>;
+  const notify = () => toast("You'r product will be available soon!");
+
+  return (
+    <div className="mother">
+      <div className="left">
+        <img src={real.image} />
+      </div>
+      <div className="right">
+        <ToastContainer
+          theme="dark"
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <div className="text-section">
+          <h1 className="t-h1">{real.title}</h1>
+          <h3>
+            <span>Price:</span> <CgDollar />
+            {real.price}
+          </h3>
+          <h4>
+            <SiChainlink />
+            Product Id: {real.id}
+          </h4>
+          <p>{real.description}</p>
+        </div>
+        <button onClick={notify}>
+          <BsBack />
+          Buy Now
+        </button>
+        <Link to="/products">
+          <button>
+            <TiArrowBack />
+            back
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default SingleProduct;
