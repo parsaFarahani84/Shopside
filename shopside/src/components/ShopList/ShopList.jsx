701236d334e62f03b3f4e801eda65a7619996c33
Parsa Frahani <90212907+parsaFarahani84@users.noreply.github.com>
2023-07-03 14:58:35 +0330

    ...

--- shopside/src/components/ShopList/ShopList.jsx
+++ shopside/src/components/ShopList/ShopList.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
 
 function ShopList() {
+  const state = useSelector((state) => state.addcard);
+  console.log(state);
   return <div>ShopList</div>;
 }
 
--- shopside/src/components/redux/store.js
+++ shopside/src/components/redux/store.js
@@ -1,6 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CounterSlice from "../counter/CounterSlice";
 
+const initialState = [];
+
 export default configureStore({
-  reducer: { counter: CounterSlice },
+  reducer: {
+    counter: CounterSlice,
+    addcard(state = initialState, action) {
+      switch (action.type) {
+        default:
+          return state;
+      }
+    },
+  },
 });
