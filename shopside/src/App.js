7a983039b1363ff1e7ccb3ead52c17411f42e5ad
Parsa Farahani <parsa.farahani84@gmail.com>
2024-09-10 23:19:05 +0330

    update

--- shopside/src/App.js
+++ shopside/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import axios from "axios";
 import { useSelector } from "react-redux";
@@ -8,19 +8,18 @@ import SingleProduct from "./components/Products/SingleProduct";
 import ShopList from "./components/ShopList/ShopList";
 import Profile from "./components/Profile/Profile";
 import { AiFillShopping } from "react-icons/ai";
-import {
-  MdOutlineProductionQuantityLimits,
-  MdSentimentSatisfiedAlt,
-} from "react-icons/md";
 import { BiSearchAlt } from "react-icons/bi";
-import { CiFilter } from "react-icons/ci";
+import { increment, addProduct } from "./components/counter/CounterSlice";
+import { useDispatch } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
   const [search, setSearch] = useState("");
   const [data, setData] = useState([]);
   const [result, setResult] = useState([]);
   const [nameCategory, setNameCategory] = useState("");
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const dispatch = useDispatch();
+
   const category = ["men's clothing", "jewelery", "electronics"];
   const count = useSelector((state) => state.counter.value);
 
@@ -47,11 +46,33 @@ function App() {
     const filteredData = data.filter((item) => item.title.includes(search));
     setResult(filteredData);
   };
+  // -------------------------------
+
+  // Add product to the cart
+  const increase = useCallback(
+    (product) => {
+      dispatch(addProduct(product)); // Dispatch addProduct action with the selected product
+      toast.success("Product added to the cart");
+    },
+    [dispatch]
+  );
 
   return (
     <BrowserRouter>
       {/* Main Header */}
       <header className="flex justify-between items-center px-4 md:px-20 py-4 bg-white shadow-md">
+        <ToastContainer
+          theme="dark"
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
         <div className="flex items-center gap-4">
           {/* Profile Button */}
           <Link to="/profile">
@@ -67,7 +88,7 @@ function App() {
           </div>
         </div>
         {/* Shopping List Button */}
-        <Link to="/shop-list">
+        <Link to="/shop-list" onClick={() => setNameCategory("")}>
           <button className="flex items-center gap-1 bg-green-600 text-pink-50 border-2 border-green-600 py-2 px-4 text-sm rounded-md hover:bg-transparent hover:text-green-600">
             <AiFillShopping className="text-lg" /> Card: {count}
           </button>
@@ -109,38 +130,40 @@ function App() {
 
       {/* Routes and Product Display */}
       {search || nameCategory ? (
-        <div className="grid grid-cols-4 gap-4 w-full mt-[5rem] px-[2rem]">
+        <div className="grid lg:grid-cols-4 grid-cols-1 gap-4 w-full mt-[5rem] px-[2rem]">
           {result &&
-            result.map((item) => (
-              <Link
-                to={`/products/${item.id}`}
-                key={item.id}
-                onClick={() => {
-                  setNameCategory("");
-                  setSearch("");
-                }}
+            result.map((product) => (
+              <div
+                key={product.id}
+                className="max-w-sm overflow-hidden bg-white h-[25rem] flex flex-col justify-between transition-all duration-300 ease-in-out rounded-[1.5rem] hover:translate-y-[-8px]"
               >
-                <div className="max-w-sm overflow-hidden bg-white h-[25rem] flex flex-col justify-between transition-all duration-300 ease-in-out rounded-[1.5rem] hover:translate-y-[-8px]">
-                  <div className="mt-[1rem]">
-                    <img
-                      className="w-full h-48 object-cover duration-300 p-[1.5rem] transition-all ease-in-out hover:p-[0.5rem]"
-                      src={item.image}
-                      alt="Product Image"
-                    />
-                    <div className="font-bold text-xl mb-2 text-gray-800 p-6">
-                      {item.title.length > 30
-                        ? item.title.slice(0, 30)
-                        : item.title}
-                    </div>
-                  </div>
-                  <div className="flex justify-between items-center px-[2rem] pb-[1rem]">
-                    <p className="text-gray-600 text-base flex items-center">
-                      Price: ${item.price}
-                    </p>
-                    <span className="flex items-center">click to buy</span>
+                <div>
+                  <img
+                    className="w-full h-48 object-cover duration-300 p-[1.5rem] transition-all ease-in-out hover:p-[0.5rem]"
+                    src={product.image}
+                    alt="Product Image"
+                  />
+                  <div className="font-bold text-xl mb-2 text-gray-800 p-6">
+                    {product.title.length > 30
+                      ? product.title.slice(0, 30)
+                      : product.title}
                   </div>
                 </div>
-              </Link>
+                <div className="flex justify-between items-center px-[2rem] pb-[1rem]">
+                  <p className="text-gray-600 text-base flex items-center">
+                    Price: ${product.price}
+                  </p>
+                  <button
+                    className="flex items-center bg-green-600 text-pink-50 border-2 border-green-600 py-2 px-4 text-sm rounded-md hover:bg-transparent hover:text-green-600"
+                    onClick={() => {
+                      dispatch(increment());
+                      increase(product); // Pass the product to increase function
+                    }}
+                  >
+                    click to buy
+                  </button>
+                </div>
+              </div>
             ))}
         </div>
       ) : (
